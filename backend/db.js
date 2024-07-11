8f1d0e4aaa64bdea51355da59ffa10093bd1596d
prabhavsetia <prabhav.setia21@gmail.com>
2024-07-11 17:40:11 +0530

    adding validations

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/ethernotes";
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,).then((res) => {
         console.log("Database connected");
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Create a user using: POST "/api/auth". Dosent require auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
-    // res.json([])
-})
+router.post('/', [
+    body('name', 'enter a valid name').isLength({ min: 3 }),
+    body('email', 'enter a valid email').isEmail(),
+    body('password', ' password must be atleast 8 characters').isLength({ min: 8 })]
+    , async (req, res) => {
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.send({ errors: result.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user =>res.json(user))
+        .catch(err=> {console.log(err)
+            res.json({error: 'User is alredy registered',message: err.message})
+        })
+    })
 
 module.exports = router 
