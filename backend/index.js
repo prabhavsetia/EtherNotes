2d7cb33090729da62abf757a556266bcc3dd490a
prabhavsetia <prabhav.setia21@gmail.com>
2024-07-11 16:14:20 +0530

    authintication

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const connectToMongo = require("./db");
+const express = require('express')
+const app = express()
+const port = 3000
+
+connectToMongo();
+app.use(express.json());
+// Available Routs
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes.js'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}/`)
+})
