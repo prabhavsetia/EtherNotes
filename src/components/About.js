61e5b9012a0d72159ad052f82fce38d1157f92cd
prabhavsetia <prabhav.setia21@gmail.com>
2024-07-14 14:34:42 +0530

    form: add a note | use location method

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,9 @@
 import React,{ useContext, useEffect } from 'react'
 import noteContext from '../context/notes/NoteContext'
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(()=>{
-    a.update()
-  })
   return (
     <div>
-      This is About {a.state.name} and his age is {a.state.age}
+      This is About 
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,23 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is Home
+      <div className='container my-3'>
+        <h1>Add a note</h1>
+        <form className="my-3">
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Title</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button type="button" className="btn btn-primary">Submit</button>
+        </form >
+      </div>
+      <div className="container my-3">
+        <h2>Your Notes</h2>
+      </div>
     </div>
   )
 }
